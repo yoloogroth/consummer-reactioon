84bc17d6eae685e02c872b3818748f344e2b157f
adsoftsito <adsoft@live.com.mx>
2023-05-14 23:18:00 -0600

    add submit

--- hello.py
+++ hello.py
@@ -1,11 +1,31 @@
 from pyspark.sql import SparkSession
 
+#uri = "mongodb+srv://adsoft:adsoft-sito@cluster0.kzghgph.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://adsoft:adsoft-sito@cluster0.kzghgph.mongodb.net/memes?retryWrites=true&w=majority"
+
 if __name__ == "__main__":
     spark = SparkSession\
         .builder\
         .appName("hello_spark")\
+        .config("spark.mongodb.read.connection.uri", uri) \
+        .config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2_12-10.1.1:3.3.2')\
+        .config('spark.jars.packages', 'org.mongodb.spark:bson-3.8.1:3.3.2')\
         .getOrCreate()
 
+#        config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.0").\
+
     print("Hello spark world, welcome adsoft ... ")
+#    df = spark.read.format("mongodb").load()
+#com.mongodb.spark.sql.DefaultSource
+    df = spark.read\
+    .format("mongodb")\
+    .option("uri", uri)\
+    .option("database","memes")\
+    .option("collection", "memes_info")\
+    .load()
+
+    #.format("com.mongodb.spark.sql.DefaultSource")\
+
+    df.printSchema()
 
     spark.stop()
--- test.sh
+++ test.sh
@@ -1,3 +1,21 @@
 echo "submitting spark job..."
 # export EXAMPLE_JAR=$(kubectl exec -ti --namespace kafka-adsoftsito my-spark-worker-0 -- find examples/jars/ -name 'spark-example*\.jar' | tr -d '\r')  
-kubectl exec -ti --namespace kafka-adsoftsito my-spark-worker-0 -- spark-submit --master spark://my-spark-master-svc:7077 --class org.apache.spark.examples.SparkPi https://raw.githubusercontent.com/adsoftsito/kafka-consumer-python/master/hello.py
+#kubectl exec -ti --namespace kafka-adsoftsito my-spark-worker-0 -- spark-submit --master spark://my-spark-master-svc:7077 --class org.apache.spark.examples.SparkPi https://raw.githubusercontent.com/adsoftsito/kafka-consumer-python/master/hello.py
+kubectl cp hello.py my-spark-worker-0:/tmp/hello.py
+kubectl cp mongodb-driver-3.8.1.jar           my-spark-worker-0:/opt/bitnami/spark/jars/
+kubectl cp mongodb-driver-core-3.8.1.jar      my-spark-worker-0:/opt/bitnami/spark/jars/
+kubectl cp mongo-spark-connector_2.12-10.1.1.jar  my-spark-worker-0:/opt/bitnami/spark/
+kubectl cp bson-3.8.1.jar                         my-spark-worker-0:/opt/bitnami/spark/jars/
+
+#kubectl cp hello.py my-spark-master-0:/tmp/hello.py
+#kubectl cp mongodb-driver-3.8.1.jar           my-spark-master-0:/opt/bitnami/spark/
+#kubectl cp mongodb-driver-core-3.8.1.jar      my-spark-master-0:/opt/bitnami/spark/
+#kubectl cp mongo-spark-connector_2.12-10.1.1.jar  my-spark-master-0:/opt/bitnami/spark/
+#kubectl cp bson-3.8.1.jar                     my-spark-master-0:/opt/bitnami/spark/
+
+
+#kubectl exec -ti --namespace kafka-adsoftsito my-spark-worker-0 -- spark-submit  --jars mongo-spark-connector_2.13-10.jar --class bson-3.8.1.jar  --master spark://my-spark-master-svc:7077  /tmp/hello.py
+
+
+kubectl exec -ti --namespace kafka-adsoftsito my-spark-worker-0 -- spark-submit --jars mongodb-driver-3.8.1.jar --class mongodb-driver-core-3.8.1.jar --jars mongo-spark-connector_2.12-10.1.1.jar --class bson-3.8.1.jar  --master spark://my-spark-master-svc:7077  /tmp/hello.py
+
